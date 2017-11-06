526f64a23ed5d2aa978a70285423a6e3fa394957
robertosalazare <jrse95@gmail.com>
2017-11-05 23:46:43 -0700

    Arregladas algunas vistas

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'app/shared/services/user.service';
+import { User } from 'app/shared/services/user';
 
 @Component({
     selector: 'app-sidebar',
     templateUrl: './sidebar.component.html',
     styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
     isActive = false;
     showMenu = '';
+    user: User = new User();
+
+    constructor(
+        private us: UserService
+    ) { }
+
+    ngOnInit() {
+        this.us.getUserObservable().subscribe((user: User) => {
+            this.user = user
+        })
+    }
+
     eventCalled() {
         this.isActive = !this.isActive;
     }
+
     addExpandClass(element: any) {
         if (element === this.showMenu) {
             this.showMenu = '0';
